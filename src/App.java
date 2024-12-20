18e80ce7b87507c87a0e07999158d8f795ce8430
Azhi Mahmood <azhi.3970166@gmail.com>
2024-12-20 23:07:21 +0300

    finish the parser class

--- src/App.java
+++ src/App.java
@@ -1,11 +1,16 @@
-import java.util.List;
+import java.util.*;
 
 public class App {
     public static void main(String[] args) {
-        Lexer lexer = new Lexer("id + id * ( id )");
-        List<Token> tokens = lexer.tokenize();
-        for (Token t : tokens) {
-            System.out.println(t);
-        }
+        String input = "a + a";
+
+
+        TopDownParser topDown = new TopDownParser(input);
+        boolean topDownResult = topDown.parse();
+        System.out.println("Top-Down Parser result for \"" + input + "\": " + topDownResult);
+
+        BottomUpParser bottomUp = new BottomUpParser(input);
+        boolean bottomUpResult = bottomUp.parse();
+        System.out.println("Bottom-Up Parser result for \"" + input + "\": " + bottomUpResult);
     }
 }
